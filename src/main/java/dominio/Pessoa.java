56a0efd97138b5c38993366c72322ae3b94db17f
João Antonio <joao.vision.ae@gmail.com>
2025-03-31 15:56:50 -0300

    Conectando com o banco de dados usando hibernate ( JPA puro )

--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -1,13 +1,28 @@
 package dominio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+import javax.persistence.*;
 
+
+@Entity
 public class Pessoa implements Serializable {
 
-        private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
         private Integer id;
+
         private String nome;
         private String email;
+
+    public Pessoa(){
+
+    }
 
     public Pessoa(Integer id, String nome, String email) {
         this.id = id;
@@ -37,5 +52,10 @@ public class Pessoa implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + " id = " + id + ", nome = '" + nome + '\'' + ", email = '" + email + '\'';
     }
 }
